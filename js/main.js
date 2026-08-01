bfd19de38273ddf4614de527697a3e369f1f60dc
finaliteration <hungbg120@gmail.com>
2026-08-01 18:51:48 +0700

    h

--- js/main.js
+++ js/main.js
@@ -22,40 +22,48 @@ const router = VueRouter.createRouter({
 });
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
 
-window.addEventListener("load", () => {
+Vue.nextTick(initAppleUnderline);
+
+router.afterEach(() => {
+
+    Vue.nextTick(initAppleUnderline);
+
+});
+
+function initAppleUnderline() {
 
     const nav = document.querySelector(".nav");
-
     const underline = document.querySelector(".apple-underline");
+
+    if (!nav || !underline) return;
 
     const tabs = nav.querySelectorAll(".nav__tab");
 
-    function move(el){
+    function move(el) {
 
         underline.style.left = el.offsetLeft + "px";
-
         underline.style.width = el.offsetWidth + "px";
 
     }
 
-    const active = nav.querySelector(".router-link-active");
-
-    if(active) move(active);
-
-    tabs.forEach(tab=>{
-
-        tab.addEventListener("mouseenter",()=>move(tab));
-
-    });
-
-    nav.addEventListener("mouseleave",()=>{
+    function updateActive() {
 
         const active = nav.querySelector(".router-link-active");
 
-        if(active) move(active);
+        if (active) move(active);
+
+    }
+
+    updateActive();
+
+    tabs.forEach(tab => {
+
+        tab.onmouseenter = () => move(tab);
 
     });
 
-});
+    nav.onmouseleave = updateActive;
+
+}
