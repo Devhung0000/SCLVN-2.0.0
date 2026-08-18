a5f5ec977fc368cf48416f7af9b9036a3c92524c
giangdelt <thedeltreal@gmail.com>
2026-08-18 14:57:50 +0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,16 @@ import List from './pages/List.js?a=1';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Statistics from './pages/Statistics.js';
- 
+import Login from './pages/Login.js';
+import Submit from './pages/Submit.js';
+import Admin from './pages/Admin.js';
+
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/statistics', component: Statistics },
+    { path: '/login', component: Login },
+    { path: '/submit', component: Submit },
+    { path: '/admin', component: Admin },
 ];
