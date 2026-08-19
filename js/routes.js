28df7e7a7d37be88b877e37393389e6e48a1e131
giangdelt <thedeltreal@gmail.com>
2026-08-19 15:13:14 +0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -7,7 +7,6 @@ import { RegisterPage as Register } from './pages/Register.js'; // ✅ Đã sử
 import Submit from './pages/Submit.js';
 import Admin from './pages/Admin.js';
 import Profile from './pages/Profile.js';
-import PublicProfile from './pages/PublicProfile.js';
 
 export default [
     { path: '/', component: List },
